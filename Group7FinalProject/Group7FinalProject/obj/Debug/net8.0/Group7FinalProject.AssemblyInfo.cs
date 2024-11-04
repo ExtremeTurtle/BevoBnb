dbc1b0b36386db455ce58f613b269ce8e27cabe2
ExtremeTurtle <lukehartfield@gmail.com>
2024-11-04 17:54:45 -0600

    Reservation Class
    Coded Reservation Class

--- Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
+++ Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b5d87e0b1f3eec7885ede147dce50caf84cb35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7785b73a2c59353c5d25ef9460b91ccabcb3edf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
