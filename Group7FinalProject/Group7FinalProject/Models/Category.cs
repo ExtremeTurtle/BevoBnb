08b38f68ab6c5c742f8d7e7c960d5231013fc717
BSchlosser7 <bradyschlosser@gmail.com>
2024-11-05 16:36:14 -0600

    Review and Category
    Title

--- Group7FinalProject/Group7FinalProject/Models/Category.cs
+++ Group7FinalProject/Group7FinalProject/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace Group7FinalProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Group7FinalProject.Models
 {
     public class Category
     {
+        [Display(Name = "Category ID")]
+        public Int32 CategoryID { get; set; }
+
+        [Display(Name = "Category Name")]
+        public String CategoryName { get; set; }
+
     }
 }
--- Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
+++ Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc1b0b36386db455ce58f613b269ce8e27cabe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb5b2f5b276a0d370dc995c2562f866b4392a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
