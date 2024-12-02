4df9ef8a4ffa3a34de893982df3ab4043ba433f7
ExtremeTurtle <lukehartfield@gmail.com>
2024-12-02 11:57:48 -0600

    Push?

--- Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
+++ Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14e701f62acd75bc675c63513e5c7cbea97e24b")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ff7f6076e8024e3521a684b505c2e13d77eaf7")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
