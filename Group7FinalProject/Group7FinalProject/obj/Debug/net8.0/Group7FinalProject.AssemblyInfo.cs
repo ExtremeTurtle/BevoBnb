5dc454d4344ee3f120205ea1270181537edbd134
crosenblum0 <crosenblum0@gmail.com>
2024-12-08 18:28:10 -0500

    minor visuals

--- Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
+++ Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f37a1679779d61c63a5933e9cee9fdea1f6571")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59b28f4409bc6e008494960c43a6f8b924923cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
