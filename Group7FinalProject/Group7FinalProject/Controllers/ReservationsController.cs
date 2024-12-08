9ca7d9d26a580c860760836f258ce0d252542ce3
ExtremeTurtle <lukehartfield@gmail.com>
2024-12-08 16:49:31 -0600

    push

--- Group7FinalProject/Group7FinalProject/Controllers/ReservationsController.cs
+++ Group7FinalProject/Group7FinalProject/Controllers/ReservationsController.cs
@@ -29,7 +29,7 @@ namespace Group7FinalProject.Controllers
         }
 
         // GET: Reservations
-        [Authorize(Roles = "Customer, Admin")]
+        [Authorize(Roles = "Customer, Host, Admin")]
         public async Task<IActionResult> Index()
         {
             // Set up a list of reservations to display
--- Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
+++ Group7FinalProject/Group7FinalProject/obj/Debug/net8.0/Group7FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a221c4e2b5a5f6b1edc944677d7517502afc7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f37a1679779d61c63a5933e9cee9fdea1f6571")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group7FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
